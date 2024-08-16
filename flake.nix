bb111bf21ca0afaa340b6d0dda96be4cd9b0d926
thomasjm <tom@codedown.io>
2024-08-16 03:05:37 -0700

    Trying to add a Nix flake and build things

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gitignore = {
+    url = "github:hercules-ci/gitignore.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  outputs = { self, flake-utils, gitignore, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              gcc
+            ];
+          };
+        }
+    );
+}
